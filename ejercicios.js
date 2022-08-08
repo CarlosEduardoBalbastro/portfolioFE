bf46116f9b02747e9aa3a141f72253b104f19ead
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2022-08-08 20:32:28 -0300

    modificacion ejercicios lunes 8 de Agosto

--- ejercicios.js
+++ ejercicios.js
@@ -1,4 +1,15 @@
-function myFunction(){
-    document.getElementById('sobreMiHiden').style.display = 'd-block';
-    console.log('vinculando con exito el boton')
+for (let i = 0; i <= 10; i++){
+    console.log('Numero: ' + i)
 }
+
+let resultado = 'Hacer: ';
+
+let y = 0;
+
+do {
+    y = y + 1;
+    console.log(resultado + y); 
+}while(y<5)
+
+
+
